12bbfdd390bf48ce9d03d376385291d5fa8aea12
Umair Ali <u3kerio@gmail.com>
2026-03-08 10:36:58 +0500

    calendar

---
+++ src/components/MeetingCalendar.jsx
@@ -0,0 +1,29 @@
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+function MeetingCalendar() {
+
+  const handleDateClick = (info) => {
+    alert("Selected date: " + info.dateStr);
+  };
+
+  return (
+    <div>
+      <h2>Meeting Calendar</h2>
+
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        dateClick={handleDateClick}
+        events={[
+          { title: "Investor Meeting", date: "2026-03-10" }
+        ]}
+      />
+
+    </div>
+  );
+}
+
+export default MeetingCalendar;
